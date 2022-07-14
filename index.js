09cf9cdc77dd6cc12e1aac7b23a77e41e22845c6
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-07-14 09:39:48 -0500

    Consulta de datos

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const conexion = createConnection(
     }
 )
 
+/* Consulta de todos los usuarios de una tabla en mi BD */
+conexion.query('SELECT * FROM personas', (error, fila)=>{
+    if(error) throw error
+    fila.forEach(
+        fila => console.log(fila)
+    )
+    console.log(`Tu tabla tiene un total de: ${fila.length} datos`)
+})
